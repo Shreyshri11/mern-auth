f44eead487925de0190f6ab434a9657ee7321d1e
Shreyshri11 <shreysrivastava175@gmail.com>
2025-03-24 12:06:55 +0530

    Add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
+    //in order to store the user information in the local storage of the browser so that it remain signed-in
     <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
         <App />
+        </PersistGate>
     </Provider>
 );
