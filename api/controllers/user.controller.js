df42a62fbb885a6cc67f8f75d61c56d000a3c5c7
Shreyshri11 <shreysrivastava175@gmail.com>
2025-03-31 15:50:49 +0530

    create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,43 @@
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import user from "../models/user.model.js";
+
 export const test = (req, res) => {
     res.json({
-        message: 'API is working!',
+        message: "API is working!",
     });
+};
+
+//update user
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You can only update your account!"));
+    }
+
+    try {
+        if (req.body.password) {
+            // Hash the password before saving it to the database
+            req.body.password = await bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await user.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    profilePicture: req.body.profilePicture,
+                }
+            },
+            { new: true } // Return the updated document, to see the updated user not the old one
+        ) ;
+
+        // send the user info to the client side 
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest) ;
+    } catch (error) {
+        next(error);
+    }
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -19,6 +20,8 @@ const app = express();
 
 //middleware
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port} `);
