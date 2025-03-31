eb567343bcc1d8d1fb444fef3be4f5765545636f
Shreyshri11 <shreysrivastava175@gmail.com>
2025-03-31 20:05:21 +0530

    complete update user functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -26,7 +26,7 @@ export default function OAuth() {
             });
             const data = await res.json();
             dispatch(signInSuccess(data));
-            navigate("/profile");
+            navigate("/");
         } catch (error) {
             console.log("could not login with google", error);
         }
