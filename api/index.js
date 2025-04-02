6b8aca8ec1378217d5753fe6d6db71693e9a015c
Shreyshri11 <shreysrivastava175@gmail.com>
2025-04-02 11:19:20 +0530

    Add preproduction changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -16,9 +17,18 @@ mongoose
         console.error(error.message);
     });
 
+const __dirname = path.resolve(); // this is going to find dynamic directory name
+
 const app = express();
 
 //middleware
+
+app.use(express.static(path.join(__dirname, "/client/dist"))); // this is going to serve the static files from the build folder
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+} ) ;// this is going to serve the index.html file from the build folder
+
 app.use(express.json());
 
 app.use(cookieParser());
